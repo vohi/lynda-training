1454eaa5198668c12263b30eeb40892ffd2e644d
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-07 15:21:48 +0200

    String manipulation exercise

--- Exercise Files/Ch03/03_02/strings.go
+++ Exercise Files/Ch03/03_02/strings.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
-//	"strings"
+	"strings"
 )
 
 func main() {
-	
+	str1 := "An implicitly typed string"
+	fmt.Printf("str1: %v:%T\n", str1, str1)
+	str2 := "An expliclty typed string"
+	fmt.Printf("str2: %v:%T\n", str2, str2)
+	fmt.Println(strings.ToUpper(str1))
+	fmt.Println(strings.Title(str1))
+
+	lValue := "hello"
+	uValue := "HELLO"
+
+	fmt.Println("Equal?", (lValue == uValue))
+	fmt.Println("Equal non-case-sensitive?", strings.EqualFold(lValue, uValue))
+
+	fmt.Println("Contains exp?", strings.Contains(str1, "exp"))
+	fmt.Println("Contains exp?", strings.Contains(str2, "exp"))
 }
