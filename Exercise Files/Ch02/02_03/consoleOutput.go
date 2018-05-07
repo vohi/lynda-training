e0ef94108f53f37a16f30aebc77b8cd8d3555a00
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-07 14:38:35 +0200

    Return handling

--- Exercise Files/Ch02/02_03/consoleOutput.go
+++ Exercise Files/Ch02/02_03/consoleOutput.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	
+
 	str1 := "The quick red fox"
 	str2 := "jumped over"
 	str3 := "the lazy brown dog."
 	//aNumber := 42
 	//isTrue := isTrue
-		
+
+	stringLength, err := fmt.Println(str1, str2, str3)
+
+	if err == nil {
+		fmt.Println("String length:", stringLength)
+	}
 }
