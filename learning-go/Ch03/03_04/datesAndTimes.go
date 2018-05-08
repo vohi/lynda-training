6315002409592801eb9ad408e2b50b5b6606856f
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-08 10:40:21 +0200

    Use package level functions for time/date

--- learning-go/Ch03/03_04/datesAndTimes.go
+++ learning-go/Ch03/03_04/datesAndTimes.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	
+	// package level functions ~ static methods
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Printf("Go launched at %s\n", t)
+
+	now := time.Now()
+	fmt.Printf("The time now is %s\n", now)
 }
