ac8baf85003035c59317a28f2d71e048b5651601
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-08 10:50:26 +0200

    Date/time formatting

--- learning-go/Ch03/03_04/datesAndTimes.go
+++ learning-go/Ch03/03_04/datesAndTimes.go
@@ -12,4 +12,18 @@ func main() {
 
 	now := time.Now()
 	fmt.Printf("The time now is %s\n", now)
+
+	// type methods to operate on objects
+	fmt.Println("The month is", t.Month())
+	fmt.Println("The day is", t.Day())
+	fmt.Println("The weekday is", t.Weekday())
+
+	tomorrow := now.AddDate(0, 0, 1)
+	fmt.Printf("Tomorrow is %v, %v %v, %v\n", tomorrow.Weekday(), tomorrow.Month(), tomorrow.Day(), tomorrow.Year())
+
+	// reference date is 1-2-3-4-5-6-7: January 2nd, 03:45; 2006; GMT-7
+	longFormat := "Monday, January 2, 2006"
+	shortFormat := "2.1.06" // day-month-year
+	fmt.Println("Tomorrow is", tomorrow.Format(longFormat))
+	fmt.Println("Tomorrow is", tomorrow.Format(shortFormat))
 }
