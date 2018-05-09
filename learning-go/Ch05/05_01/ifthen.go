5969b7bb8cfa637610f54c0fc53c8c55f98d7ec2
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-09 13:58:51 +0200

    Conditional with locally scoped variable

--- learning-go/Ch05/05_01/ifthen.go
+++ learning-go/Ch05/05_01/ifthen.go
@@ -4,7 +4,16 @@ import "fmt"
 
 func main() {
 
-	var x float64 = 42
+	// var x float64 = 42
 	var result string
-	
+
+	if x := 42; x < 0 {
+		result = "less than zero"
+	} else if x == 0 {
+		result = "equal zero"
+	} else {
+		result = "greater than zero"
+	}
+
+	fmt.Println("x is", result)
 }
