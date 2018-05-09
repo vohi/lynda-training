80cf441008a400de7938df6e49b56751b1b31170
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-09 14:22:49 +0200

    Compact switch statement

--- learning-go/Ch05/05_02/switch.go
+++ learning-go/Ch05/05_02/switch.go
@@ -3,13 +3,35 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"	
-) 
+	"time"
+)
 
 func main() {
 
 	rand.Seed(time.Now().Unix())
-	dow := rand.Intn(6) + 1
-	fmt.Println("Day", dow)
 
+	result := ""
+	switch dow := rand.Intn(6) + 1; dow {
+	case 6:
+		result = "Saturday"
+	case 7:
+		result = "Sunday"
+	default:
+		result = "Weekday"
+	}
+
+	fmt.Println("It's a", result)
+
+	x := -42
+
+	switch {
+	case x < 0:
+		result = "Less than zero"
+		// fallthrough // optional fallthrough avoids need for break
+	case x == 0:
+		result = "Equal zero"
+	default:
+		result = "Greater than zero"
+	}
+	fmt.Println("It's a", result)
 }
