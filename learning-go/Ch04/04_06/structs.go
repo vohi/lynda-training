4f4f2770dcf53e09c164c756e9711e5f5d52f763
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-09 12:25:26 +0200

    Basic customer data structure

--- learning-go/Ch04/04_06/structs.go
+++ learning-go/Ch04/04_06/structs.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
-func main() {
+type Dog struct {
+	Breed  string
+	Weight int
+}
 
+func main() {
+	poodle := Dog{"Poodle", 34}
+
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
 }
