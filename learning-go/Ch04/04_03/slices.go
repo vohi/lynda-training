642dc04b5d692f8358ab74f9ab3a2140f6d78c02
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-09 11:02:56 +0200

    Basic slice operations

--- learning-go/Ch04/04_03/slices.go
+++ learning-go/Ch04/04_03/slices.go
@@ -5,5 +5,27 @@ import (
 )
 
 func main() {
-	
+	var colors = []string{"Red", "Green", "Blue"}
+	fmt.Println(colors)
+
+	colors = append(colors, "Purple")
+	fmt.Println(colors)
+
+	colors = append(colors[1:len(colors)]) // len(colors) would be the default
+	fmt.Println(colors)
+
+	colors = append(colors[:len(colors)-1]) // 0 is the default
+	fmt.Println(colors)
+
+	numbers := make([]int, 5, 5) // type, size, capacity (defaults to size)
+	numbers[0] = 1
+	numbers[1] = 7
+	numbers[2] = 32
+	numbers[3] = 12
+	numbers[4] = 156
+	fmt.Println(numbers)
+
+	numbers = append(numbers, 235)
+	fmt.Println(numbers)
+	fmt.Println(cap(numbers)) // prints 10 - go's growth strategy for slices
 }
