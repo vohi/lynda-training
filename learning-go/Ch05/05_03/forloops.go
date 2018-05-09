9ffbdc66facead233e2de9b542f967d7c1ec845b
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-09 14:32:55 +0200

    For loop, goto

--- learning-go/Ch05/05_03/forloops.go
+++ learning-go/Ch05/05_03/forloops.go
@@ -6,8 +6,22 @@ func main() {
 
 	sum := 1
 	fmt.Println("Sum:", sum)
-	
+
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
-	
+
+	for i := range colors {
+		fmt.Println(i)
+	}
+
+	for sum < 1000 { // while loop
+		sum += sum
+		if sum > 900 {
+			goto endofprogram
+		}
+	}
+	fmt.Println(sum)
+
+endofprogram:
+	fmt.Println("The end")
 }
