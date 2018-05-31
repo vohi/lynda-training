7a21b82c5f233d6bfd4dcadcf6aea72642571d16
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-31 15:12:30 +0200

    Simple multi-return function

--- learning-go/Ch06/06_02/multiReturn.go
+++ learning-go/Ch06/06_02/multiReturn.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	n1, l1 := FullName("Zaphod", "Beeblebrox")
+	fmt.Printf("Fullname: %v, length: %v", n1, l1)
 
+}
+
+func FullName(f, l string) (string, int) {
+	full := f + " " + l
+	length := len(full)
+	return full, length
 }
