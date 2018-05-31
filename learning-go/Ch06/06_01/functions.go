4354a1bc5999b27520c116a69dc2896f279e379e
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-31 14:51:13 +0200

    Simple function declarations

--- learning-go/Ch06/06_01/functions.go
+++ learning-go/Ch06/06_01/functions.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	doSomething()
+	sum := addValues(23, 54)
+	fmt.Println("Sum:", sum)
+}
 
+func doSomething() {
+	fmt.Println("Doing something")
+}
+
+func addValues(value1 int, value2 int) int {
+	return value1 + value2
 }
