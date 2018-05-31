6b66249589f344b16092a974d2bb2b593a90881c
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-31 15:18:17 +0200

    Naked return

--- learning-go/Ch06/06_02/multiReturn.go
+++ learning-go/Ch06/06_02/multiReturn.go
@@ -4,12 +4,20 @@ import "fmt"
 
 func main() {
 	n1, l1 := FullName("Zaphod", "Beeblebrox")
-	fmt.Printf("Fullname: %v, length: %v", n1, l1)
+	fmt.Printf("Fullname: %v, length: %v\n", n1, l1)
 
+	n2, l2 := FullName("Arthur", "Dent")
+	fmt.Printf("Fullname: %v, length: %v\n", n2, l2)
 }
 
 func FullName(f, l string) (string, int) {
 	full := f + " " + l
 	length := len(full)
 	return full, length
+}
+
+func FullNameNakedReturn(f, l string) (full string, length int) {
+	full = f + " " + l
+	length = len(full)
+	return // "Naked Return" - still have to have return statement
 }
