b905cc415c8182bd3af8fee8b5e220395318b02a
Volker Hilsheimer <vohi@telenordigital.com>
2018-05-31 14:58:24 +0200

    Concise, and variable function arguments

--- learning-go/Ch06/06_01/functions.go
+++ learning-go/Ch06/06_01/functions.go
@@ -6,12 +6,23 @@ func main() {
 	doSomething()
 	sum := addValues(23, 54)
 	fmt.Println("Sum:", sum)
+	sum = addAllValues(12, 54, 79)
+	fmt.Println("New Sum:", sum)
 }
 
+// lowercase initial character => functions private to the package
 func doSomething() {
 	fmt.Println("Doing something")
 }
 
-func addValues(value1 int, value2 int) int {
+func addValues(value1, value2 int) int {
 	return value1 + value2
+}
+
+func addAllValues(values ...int) int {
+	sum := 0
+	for i := range values {
+		sum += values[i]
+	}
+	return sum
 }
