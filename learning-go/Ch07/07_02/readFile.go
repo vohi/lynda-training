da6077e46461321759e5d37f1311b0ffc019b487
Volker Hilsheimer <vohi@telenordigital.com>
2018-06-04 16:33:42 +0200

    Basic file operations, and ignore output files

--- learning-go/Ch07/07_02/readFile.go
+++ learning-go/Ch07/07_02/readFile.go
@@ -7,8 +7,15 @@ import (
 
 func main() {
 
-	fileName := "./hello.txt"	
+	fileName := "./hello.txt"
 
+	content, err := ioutil.ReadFile(fileName)
+	checkError(err)
+
+	fmt.Println("Read from file:", content)
+
+	result := string(content)
+	fmt.Println("Read from file:", result)
 }
 
 func checkError(err error) {
