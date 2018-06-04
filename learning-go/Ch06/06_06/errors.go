6838fbe8ebe54979711be7fc85bee429704e5aee
Volker Hilsheimer <vohi@telenordigital.com>
2018-06-04 12:33:36 +0200

    Basic error handling

--- learning-go/Ch06/06_06/errors.go
+++ learning-go/Ch06/06_06/errors.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
 	f, err := os.Open("filename.ext")
+
+	if err == nil {
+		fmt.Println(f)
+	} else {
+		fmt.Println(err.Error())
+	}
+
+	// custom error object
+	myError := errors.New("My error string")
+	fmt.Println(myError)
 
 	// attendance := map[string]bool{
 	// 	"Ann": true,
