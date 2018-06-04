7b4f37cb160b9210a852487110efa7303dab7895
Volker Hilsheimer <vohi@telenordigital.com>
2018-06-04 11:59:27 +0200

    Simple methods to a struct

--- learning-go/Ch06/06_04/methods.go
+++ learning-go/Ch06/06_04/methods.go
@@ -3,11 +3,24 @@ package main
 import "fmt"
 
 type Dog struct {
-	Breed string
+	Breed  string
 	Weight int
-	Sound string
+	Sound  string
+}
+
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
 }
 
 func main() {
+	poodle := Dog{
+		"Poodle",
+		37,
+		"Woof"}
+	fmt.Println(poodle)
 
+	poodle.Speak()
+
+	poodle.Sound = "Arf"
+	poodle.Speak()
 }
