f6d37362098a43f4029e6911d74e4557bb6aaa03
Volker Hilsheimer <vohi@telenordigital.com>
2018-06-06 16:14:46 +0200

    Simple HTTP GET request with response handling

--- learning-go/Ch07/07_04/webRequest.go
+++ learning-go/Ch07/07_04/webRequest.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
 
 	url := "http://services.explorecalifornia.org/json/tours.php"
 
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response type: %T\n", response)
+
+	defer response.Body.Close()
+
+	bytes, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(bytes)
+	fmt.Print(content)
 }
