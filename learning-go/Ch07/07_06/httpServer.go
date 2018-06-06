270708e9cd07335bed800371340acaf2a0dfb6c7
Volker Hilsheimer <vohi@telenordigital.com>
2018-06-06 17:06:42 +0200

    Trivial HTTP server

--- learning-go/Ch07/07_06/httpServer.go
+++ learning-go/Ch07/07_06/httpServer.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func main() {
+type Hello struct {
+}
 
+func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello from the GO web server!</h1>")
+}
+
+func main() {
+	var h Hello
+
+	err := http.ListenAndServe("localhost:4000", h)
+	checkError(err)
 }
 
 func checkError(err error) {
