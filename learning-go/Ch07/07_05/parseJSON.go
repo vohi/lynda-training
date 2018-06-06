72c3c9d956b058bd27b1a15537b47e62be36134c
Volker Hilsheimer <vohi@telenordigital.com>
2018-06-06 16:53:40 +0200

    Using type introspection to parse JSON

--- learning-go/Ch07/07_05/parseJSON.go
+++ learning-go/Ch07/07_05/parseJSON.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
 	"io/ioutil"
-	// "encoding/json"
-	// "strings"
-	// "math/big"
+	"math/big"
+	"net/http"
+	"strings"
 )
+
+type Tour struct {
+	Name, Price string
+}
 
 func main() {
 
 	url := "http://services.explorecalifornia.org/json/tours.php"
 	content := contentFromServer(url)
 
-	fmt.Println(content)
+	tours := toursFromJSON(content)
 
+	for _, tour := range tours {
+		price, _, _ := big.ParseFloat(tour.Price, 10, 2, big.ToZero) // base, precision, rounding mode
+		fmt.Printf("%v ($%.2f)\n", tour.Name, price)
+	}
 }
 
 func checkError(err error) {
@@ -25,10 +33,10 @@ func checkError(err error) {
 }
 
 func contentFromServer(url string) string {
-	
+
 	resp, err := http.Get(url)
 	checkError(err)
-	
+
 	defer resp.Body.Close()
 	bytes, err := ioutil.ReadAll(resp.Body)
 	checkError(err)
@@ -36,3 +44,19 @@ func contentFromServer(url string) string {
 	return string(bytes)
 }
 
+func toursFromJSON(content string) []Tour {
+	tours := make([]Tour, 0, 20) // initial capacity 20
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	checkError(err)
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		checkError(err)
+		tours = append(tours, tour)
+	}
+
+	return tours
+}
